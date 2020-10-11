30cb431a3d6b3eff12a5abd0387d4355a8010e2c
RodrigoJacomeli <rodrigo.devvga@gmail.com>
2020-10-11 04:19:30 -0300

    Create Routers - Test
    Criação de rotas teste

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,83 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+ } from 'react-router-dom';
 
-
-function App() {
+export default function App() {
   return (
-    <div>
-      <h4>My Clients</h4>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/ListAllClients">List All Clients</Link>
+            </li>
+            <li>
+              <Link to="/ListOneClient">List One Client</Link>
+            </li>
+            <li>
+              <Link to="/AddClient">Add Client</Link>
+            </li>
+            <li>
+              <Link to="/EditClient">Edit Client</Link>
+            </li>
+            <li>
+              <Link to="/RemoveClient">Remove Client</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/RemoveClient">
+            <RemoveClient />
+          </Route>
+          <Route path="/EditClient">
+            <EditClient />
+          </Route>
+          <Route path="/AddClient">
+            <AddClient />
+          </Route>
+          <Route path="/ListOneClient">
+            <ListOneClient />
+          </Route>
+          <Route path="/ListAllClients">
+            <ListAllClients />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+function Home() {
+  return <h2>Home</h2>
+}
+
+function ListAllClients() {
+  return <h2>List All Clients</h2>
+}
+
+function ListOneClient() {
+  return <h2>List One Client</h2>
+}
+
+function AddClient() {
+  return <h2>Add Client</h2>
+}
+
+function EditClient() {
+  return <h2>Edit Client</h2>
+}
+
+function RemoveClient() {
+  return <h2>Remove Client</h2>
+}
